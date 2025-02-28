0d1b5def3a0bb21eddd7dc19fbac5f3d6aeeb264
jatisurjayan <surjayan@reshapeapp.ai>
2025-03-01 02:14:45 +0530

    website submit query function commit

---
+++ get-all-users/index.ts
@@ -0,0 +1,47 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import {
+  getUserData,
+  loginUser,
+  updateUserData,
+  userOtpVerify,
+} from "../src/users/users.service";
+import { init } from "../src/helpers/azure-cosmosdb-mongodb";
+
+//// Main login function ------------------------------------------------------------------------------/
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    /// Building connection with the cosmos database -----------------/
+    await init();
+
+    /// replace this query _id with jsonwebtoken _id later on
+
+    /// Calling the service function ----------------------/
+    const response: { message: string; success: boolean } = await getUserData(
+      req.query._id
+    );
+    if (response.success) {
+      context.res = {
+        status: 200,
+        body: response,
+      };
+    } else {
+      context.res = {
+        status: 500,
+        body: response,
+      };
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: {
+        message: `${error.message}`,
+        success: false,
+      },
+    };
+  }
+};
+
+export default httpTrigger;
---
+++ submit-user-query/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+
+import { init } from "../src/helpers/azure-cosmosdb-mongodb";
+import { storeUserQuery } from "../src/userQuery/userquery.service";
+
+//// Main login function ------------------------------------------------------------------------------/
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    /// Building connection with the cosmos database -----------------/
+    await init();
+
+    /// replace this query _id with jsonwebtoken _id later on
+
+    /// Calling the service function ----------------------/
+    const response: { message: string; success: boolean } =
+      await storeUserQuery(req.body);
+    if (response.success) {
+      context.res = {
+        status: 200,
+        body: response,
+      };
+    } else {
+      context.res = {
+        status: 500,
+        body: response,
+      };
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: {
+        message: `${error.message}`,
+        success: false,
+      },
+    };
+  }
+};
+
+export default httpTrigger;

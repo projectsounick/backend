472b29d8a938517c2d8d59104b60237ac272607e
jatisurjayan <surjayan@reshapeapp.ai>
2025-07-18 03:22:51 +0530

    changes commit for category

--- get-productcategory/index.ts
+++ get-productcategory/index.ts
@@ -1,26 +1,27 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { init } from "../src/helpers/azure-cosmosdb-mongodb";
 import { getProductCategory } from "../src/products/category.service";
+import { verifyAndDecodeToken } from "../src/admin/admin.service";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   try {
-    // let userId: string;
-    // const authResponse = await verifyAndDecodeToken(req);
-    // if (authResponse) {
-    //   userId = authResponse;
-    // } else {
-    //   context.res = {
-    //     status: 401,
-    //     body: {
-    //       message: "Unauthorized",
-    //       success: false,
-    //     },
-    //   };
-    //   return;
-    // }
+    let userId: string;
+    const authResponse = await verifyAndDecodeToken(req);
+    if (authResponse) {
+      userId = authResponse;
+    } else {
+      context.res = {
+        status: 401,
+        body: {
+          message: "Unauthorized",
+          success: false,
+        },
+      };
+      return;
+    }
 
     await init(context);
     const { isActive, page, limit } = req.query;
@@ -28,11 +29,8 @@ const httpTrigger: AzureFunction = async function (
     const parsedIsActive =
       isActive === "true" ? true : isActive === "false" ? false : null;
 
-    const response: { message: string; success: boolean } = await getProductCategory(
-      parsedIsActive,
-      page,
-      limit
-    );
+    const response: { message: string; success: boolean } =
+      await getProductCategory(parsedIsActive, page, limit);
     if (response.success) {
       context.res = {
         status: 200,

d33157c86b7000877ab9e03f8c82a4eb80bae12f
jatisurjayan <surjayan@reshapeapp.ai>
2025-08-12 04:41:40 +0530

    changes commit for the daily notificaiton reminder

--- src/utils/staticNotificaitonContent.ts
+++ src/utils/staticNotificaitonContent.ts
@@ -6,3 +6,17 @@ export const notificationContentForNewVideo = {
   title: "New Video Uploaded",
   body: "A new video is now available for you to watch.",
 };
+export const notificationContentForImageProgress = {
+  title: "Snap Your Progress 📸",
+  body: "Show how far you’ve come! Upload today’s progress picture.",
+};
+
+export const notificationContentForWaterLog = {
+  title: "Hydration Check 💧",
+  body: "Stay refreshed! Log your water intake now.",
+};
+
+export const notificationContentForSteps = {
+  title: "Step Up Your Game 👟",
+  body: "Track your steps and keep moving towards your goal!",
+};
---
+++ trigger-daily-reminder/index.ts
@@ -0,0 +1,59 @@
+import { AzureFunction, Context } from "@azure/functions";
+import UserModel from "../src/users/user.model";
+import { sendBulkPushNotifications } from "../src/Notification/notification.service";
+import { init } from "../src/helpers/azure-cosmosdb-mongodb";
+
+import {
+  notificationContentForImageProgress,
+  notificationContentForSteps,
+  notificationContentForWaterLog,
+} from "../src/utils/staticNotificaitonContent";
+
+const timerTrigger: AzureFunction = async function (
+  context: Context,
+  myTimer: any
+): Promise<void> {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  await init(context);
+
+  let notificationContent = null;
+
+  if (hours === 4 && minutes === 40) {
+    notificationContent = notificationContentForImageProgress;
+  } else if (hours === 9 && minutes === 40) {
+    notificationContent = notificationContentForImageProgress;
+  } else if (hours === 12 && minutes === 0) {
+    notificationContent = notificationContentForWaterLog;
+  } else if (hours === 20 && minutes === 0) {
+    notificationContent = notificationContentForSteps;
+  }
+
+  if (!notificationContent) {
+    context.log("No matching notification schedule for this run.");
+    return;
+  }
+
+  // Fetch all users
+  const users = await UserModel.find({}).select("expoPushToken").lean();
+  const tokens = [
+    ...new Set(
+      users.map((user) => user.expoPushToken).filter((token) => !!token)
+    ),
+  ]; // Removes duplicates
+
+  if (tokens.length > 0) {
+    await sendBulkPushNotifications(
+      notificationContent.title,
+      notificationContent.body,
+      tokens
+    );
+    context.log(`Notification sent to ${tokens.length} unique users`);
+  } else {
+    context.log("No valid push tokens found.");
+  }
+};
+
+export default timerTrigger;

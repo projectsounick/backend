6a253831ffd49f4475b9cfcab859097dad068966
Tanmoy Manna <tanmoymanna@Tanmoys-MacBook-Air.local>
2025-08-15 03:54:56 +0530

    block unblock

---
+++ block-user/index.ts
@@ -0,0 +1,53 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { init } from "../src/helpers/azure-cosmosdb-mongodb";
+import { verifyAndDecodeToken } from "../src/admin/admin.service";
+import { blockUser } from "../src/community/community.service";
+
+//// Main login function ------------------------------------------------------------------------------/
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  try {
+    let userId: string;
+    const authResponse = await verifyAndDecodeToken(req);
+    if (authResponse) {
+      userId = authResponse;
+    } else {
+      context.res = {
+        status: 401,
+        body: {
+          message: "Unauthorized",
+          success: false,
+        },
+      };
+      return;
+    }
+    await init(context);
+    const { blockedUserId } = req.body;
+
+    const response: { message: string; success: boolean } =
+      await blockUser(userId, blockedUserId);
+    if (response.success) {
+      context.res = {
+        status: 200,
+        body: response,
+      };
+    } else {
+      context.res = {
+        status: 500,
+        body: response,
+      };
+    }
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: {
+        message: `${error.message}`,
+        success: false,
+      },
+    };
+  }
+};
+
+export default httpTrigger;
